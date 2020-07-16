0cd42201b22b3f85adca6f035513028b36f1c031
Tazro Inutano Ohta <inutano@gmail.com>
2020-07-17 01:33:44 +0900

    add ttl validator

--- biosampleplus/biosampleplus.job.sh
+++ biosampleplus/biosampleplus.job.sh
@@ -8,13 +8,13 @@ set -eux
 INPUT_DIR=$(cd $(dirname ${1}) && pwd -P)
 INPUT_JSON="${INPUT_DIR}/$(basename ${1})"
 
+module load docker
 METASRA_DOCKER_IMAGE="shikeda/metasra:1.4"
 
 #
 # Run MetaSRA
 #
 run_metasra() {
-  module load docker
   docker run --security-opt seccomp=unconfined --rm \
     -e TZ=Asia/Tokyo \
     --volume ${INPUT_DIR}:/work \
@@ -25,10 +25,28 @@ run_metasra() {
     "/work/$(basename ${INPUT_JSON})" \
     "-o" \
     "/work/$(basename ${INPUT_JSON}).ttl"
+  echo "${INPUT_JSON}.ttl"
+}
+
+validate_ttl() {
+  local ttl=${1}
+  local validation_output="${ttl}.validation"
+
+  docker run --security-opt seccomp=unconfined --rm \
+    -v $(dirname "${ttl}"):/work \
+    "quay.io/inutano/turtle-validator:v1.0" \
+    ttl \
+    /work/$(basename "${ttl}") \
+    > "${validation_output}"
+
+  if [[ $(cat "${validation_output}") == 'Validator finished with 0 warnings and 0 errors.' ]]; then
+    rm -f "${validation_output}"
+  fi
 }
 
 main() {
-  run_metasra
+  local ttl=$(run_metasra)
+  validate_ttl ${ttl}
 }
 
 #
