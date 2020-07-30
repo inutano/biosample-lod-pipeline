d9b1238f863b83cc738d89e8ad7958139ea911a3
Tazro Inutano Ohta <inutano@gmail.com>
2020-07-31 02:00:46 +0900

    refactor

--- biosample/biosample.job.sh
+++ biosample/biosample.job.sh
@@ -1,64 +1,107 @@
 #!/bin/bash
 #$ -S /bin/bash -j y
-# set -eux
+# Note: `data "+%s%N"` does not work with BSD/macOS
+N=$(date +%s%N)
+PS4='+[$((($(date +%s%N)-${N})/1000000))ms][${BASH_SOURCE}:${LINENO}]: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+set -eux
+
 module load docker
 
-XML_PATH="${1}"
-jobconf="${2}"
-WORK_DIR="$(cd $(dirname ${XML_PATH}) && pwd -P)"
+#
+# Functions
+#
+setup() {
+  XML_PATH=${1}
+  JOBCONF=${2}
+  WORKDIR="$(cd $(dirname ${XML_PATH}) && pwd -P)"
+  OUTDIR="${WORKDIR}/ttl"
 
-# For saving intermediate files
-TMPDIR="/data1/tmp/biosample-lod/biosample/ttl"
-mkdir -p "${TMPDIR}"
+  TMPDIR="/data1/tmp/biosample-lod/biosample/ttl"
+  mkdir -p "${TMPDIR}"
+}
 
-# For saving final output
-TTL_DIR="${WORK_DIR}/ttl"
-mkdir -p "${TTL_DIR}"
+get_job_param() {
+  local mode=${1}
 
-# SGE_TASK_ID=1 # For standalone testing
-job_param="$(cat ${jobconf} | awk -v id=${SGE_TASK_ID} 'NR==id')"
-job_name="biosample.$(echo ${job_param} | sed -e 's:,.*$::')"
+  case ${mode} in
+    test )
+      SGE_TASK_ID=1
+      ;;
+  esac
 
-ttl_path="${TTL_DIR}/${job_name}.ttl"
-ttl_tmp_path="${TMPDIR}/${job_name}.ttl"
+  cat ${JOBCONF} | awk -v id=${SGE_TASK_ID} 'NR==id'
+}
+
+set_job_name() {
+  local job_param=${1}
+  echo "biosample.$(echo ${job_param} | sed -e 's:,.*$::')"
+}
+
+create_xml() {
+  local job_param=${1}
+  local tmp_xml="${TMPDIR}/$(set_job_name ${job_param}).xml"
+  printf "<BioSampleSet>\n$(cat "${XML_PATH}" | sed -n "${job_param}")" > ${tmp_xml}
+  echo ${tmp_xml}
+}
+
+xml2ttl() {
+  local tmp_xml=${1}
+  local tmp_ttl="$(basename ${tmp_xml} .xml).ttl"
+  docker run \
+    --security-opt seccomp=unconfined \
+    --rm \
+    -e TZ=Asia/Tokyo \
+    --volume ${TMPDIR}:/work \
+    "quay.io/inutano/biosample_jsonld:v1.8" \
+    bs2ld \
+    xml2ttl \
+    /work/$(basename ${tmp_xml}) \
+    > "${tmp_ttl}"
+  rm -f ${tmp_xml}
+  echo "${tmp_ttl}"
+}
+
+validate_ttl() {
+  local ttl=${1}
+  local validation_output="${ttl}.validation.failed"
+
+  docker run \
+    --security-opt seccomp=unconfined \
+    --rm \
+    --user "$(id -u):$(id -g)" \
+    -v $(dirname "${ttl}"):/work \
+    "quay.io/inutano/turtle-validator:v1.0" \
+    ttl \
+    /work/$(basename "${ttl}") \
+    > "${validation_output}"
+
+  if [[ $(cat "${validation_output}") == 'Validator finished with 0 warnings and 0 errors.' ]]; then
+    rm -f "${validation_output}"
+    echo "${ttl}"
+  else
+    echo "${validation_output}"
+  fi
+}
+
+generate_turtle() {
+  setup ${1} ${2}
+  local job_param=$(get_job_param ${3})
+  local job_name=$(set_job_name ${job_param})
+  local tmp_xml=$(create_xml ${job_param})
+  local tmp_ttl=$(xml2ttl ${tmp_xml})
+  local output=$(validate_ttl ${tmp_ttl})
+  mv ${output} ${OUTDIR}
+}
+
+main() {
+  if [[ $# -gt 2 ]]; then
+    generate_turtle ${1} ${2} "test"
+  else
+    generate_turtle ${1} ${2} "productionn"
+  fi
+}
 
 #
-# Create partial XML
+# Exec
 #
-tmp_xml=${ttl_tmp_path}.xml
-printf "<BioSampleSet>\n$(cat "${XML_PATH}" | sed -n "${job_param}")" > ${tmp_xml}
-
-#
-# Convert XML to ttl on tmpdir
-#
-docker run --security-opt seccomp=unconfined --rm \
-  -e TZ=Asia/Tokyo \
-  --volume ${TMPDIR}:/work \
-  "quay.io/inutano/biosample_jsonld:v1.8" \
-  bs2ld \
-  xml2ttl \
-  /work/$(basename ${tmp_xml}) \
-  > "${ttl_tmp_path}"
-
-#
-# Validate ttl
-#
-validation_output="${ttl_tmp_path}.validation"
-valid_value='Validator finished with 0 warnings and 0 errors.'
-
-docker run --security-opt seccomp=unconfined --rm \
-  -v $(dirname "${ttl_tmp_path}"):/work \
-  "quay.io/inutano/turtle-validator:v1.0" \
-  ttl \
-  /work/$(basename "${ttl_tmp_path}") \
-  > "${validation_output}"
-
-if [[ $(cat "${validation_output}") == "${valid_value}" ]]; then
-  rm -f "${validation_output}"
-fi
-
-#
-# Move ttl file to final dest dir and remove XML
-#
-mv ${ttl_tmp_path} ${ttl_path}
-rm -f ${tmp_xml}
+main ${@}
